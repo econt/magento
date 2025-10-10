0ad13d95d66fe654c6afbcccd514da58b2ff81db
Marush Denchev <avreon@gmail.com>
2025-10-10 09:55:59 +0300

    Fix code standards issues

--- Model/Carrier/Shippingmodule.php
+++ Model/Carrier/Shippingmodule.php
@@ -102,6 +102,8 @@ class Shippingmodule extends AbstractCarrier implements CarrierInterface
     }
 
     /**
+     * Get allowed shipping methods
+     *
      * @return array
      */
     public function getAllowedMethods()
